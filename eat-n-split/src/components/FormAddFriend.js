422bd461cbfb94c44bc22c57fc9265fa08724fa9
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-07-26 12:42:03 +0200

    create static layout

---
+++ eat-n-split/src/components/FormAddFriend.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+export default function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👫 Friend name</label>
+      <input type="text" />
+
+      <label>🖼️ Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
