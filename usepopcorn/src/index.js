c4eaa9395acc53bdfa6560c86b47b5510d629822
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-07-28 17:47:25 +0200

    add component API

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+//import "./index.css";
+//import App from "./App";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={10} color="green" size={24} className="test" />
   </React.StrictMode>
 );
