af5505a0cd40263796d1c789e636cf9c067248b8
Ronnie Kaito Imagawa <ronnie.imagawa@gmail.com>
2023-08-01 19:41:47 +0200

    added loading

--- currency-converter/src/App.js
+++ currency-converter/src/App.js
@@ -6,19 +6,35 @@ export default function App() {
   const [convertFrom, setConvertFrom] = useState("USD");
   const [convertTo, setConvertTo] = useState("EUR");
   const [rate, setRate] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
+      const controller = new AbortController();
       async function fetchConversion() {
-        const res = await fetch(
-          `https://api.frankfurter.app/latest?amount=${amount}&from=${convertFrom}&to=${convertTo}`
-        );
-        const data = await res.json();
-        setRate(data.rates[convertTo]);
+        try {
+          setIsLoading(true);
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${convertFrom}&to=${convertTo}`,
+            { signal: controller.signal }
+          );
+          const data = await res.json();
+          setRate(data.rates[convertTo]);
+        } catch (err) {
+          if (err === "AbortError") {
+            return;
+          }
+        } finally {
+          setIsLoading(false);
+        }
       }
 
-      if (!amount) return;
+      if (convertTo === convertFrom) return setRate(amount);
       fetchConversion();
+
+      return function () {
+        controller.abort();
+      };
     },
     [amount, convertFrom, convertTo]
   );
@@ -29,23 +45,31 @@ export default function App() {
         type="text"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
       />
       <select
         value={convertFrom}
         onChange={(e) => setConvertFrom(e.target.value)}
+        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select value={convertTo} onChange={(e) => setConvertTo(e.target.value)}>
+      <select
+        value={convertTo}
+        disabled={isLoading}
+        onChange={(e) => setConvertTo(e.target.value)}
+      >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>{rate}</p>
+      <p>
+        {rate} {convertTo}
+      </p>
     </div>
   );
 }
